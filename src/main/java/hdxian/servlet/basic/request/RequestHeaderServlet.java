ac58f6d42f87709dce1110b25bed83c2561e2e4f
hdXian <hyun353500@naver.com>
2024-07-22 19:19:53 +0900

    feat - add guide text on basic servlet response

--- src/main/java/hdxian/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hdxian/servlet/basic/request/RequestHeaderServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -19,6 +20,8 @@ public class RequestHeaderServlet extends HttpServlet {
         printHeaders(request);
         printHeadersUtils(request);
         printEtc(request);
+        PrintWriter writer = response.getWriter();
+        writer.write("request successfully accepted. check the server console.");
     }
 
     private void printStartLine(HttpServletRequest request) {
