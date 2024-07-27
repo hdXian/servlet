598398045dc00bcf7ab191b97d180ebf78ed4894
hdXian <hyun353500@naver.com>
2024-07-27 18:13:47 +0900

    #5 section 4-1 - front controller service logic

--- src/main/java/hdxian/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hdxian/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -13,13 +13,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerV1", urlPatterns = "/front-controller/v1/*")
-public class FrontControllerV1 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
 
     private Map<String, ControllerV1> controllerMap = new HashMap<>();
 
     // put each controller in controllerMap
-    public FrontControllerV1() {
+    public FrontControllerServletV1() {
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
         controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
         controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
@@ -28,13 +28,21 @@ public class FrontControllerV1 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("FrontControllerV1.service");
+//        System.out.println("FrontControllerServletV1.service");
 
         String requestURI = request.getRequestURI();
-        System.out.println("requestURI = " + requestURI);
+//        System.out.println("requestURI = " + requestURI);
 
-//        ControllerV1 controller = controllerMap.get(requestURI);
-//        controller.process(request, response);
+        ControllerV1 controller = controllerMap.get(requestURI);
+
+        if(controller != null) {
+            controller.process(request, response);
+
+        }
+        else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//            return;
+        }
 
     }
 }
