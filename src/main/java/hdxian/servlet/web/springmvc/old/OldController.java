9208acf14ae51055a82ecd84eeeb4ddd2583da9c
hdXian <hyun353500@naver.com>
2024-08-01 00:17:45 +0900

    #7 section 5-1 - make example code of HandlerMapping, HandlerAdapter

---
+++ src/main/java/hdxian/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,19 @@
+package hdxian.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// register bean by url
+// HandlerMapping - BeanNameUrlHandlerMapping
+// HandlerAdapter - SimpleControllerHandlerAdapter (ex. implements interface Controller)
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
