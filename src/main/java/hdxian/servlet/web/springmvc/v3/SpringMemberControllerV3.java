1444006872eacdf063b4febeabd426b757fea799
hdXian <hyun353500@naver.com>
2024-08-01 18:07:57 +0900

    #7 section 5-5 - return viewName as String, use @RequstParam, Model model - v3

---
+++ src/main/java/hdxian/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,45 @@
+package hdxian.servlet.web.springmvc.v3;
+
+import hdxian.servlet.domain.member.Member;
+import hdxian.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private final MemberRepository repository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @RequestMapping("/save")
+    public String save(@RequestParam("username") String username,
+                       @RequestParam("age") int age,
+                       Model model) {
+
+        // generate and save new member
+        Member member = new Member(username, age);
+        repository.save(member);
+
+        model.addAttribute("member", member);
+        return "save";
+    }
+
+    // Mapped "/springmvc/v2/members" (class level default)
+    @RequestMapping
+    public String members(Model model) {
+        List<Member> members = repository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
+
+}
